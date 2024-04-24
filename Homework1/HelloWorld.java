37d3abe7af9f3de893c0151471c7aa514d48438f
Faith Winford <141306476+fwinford@users.noreply.github.com>
2024-04-24 00:05:13 -0400

    added HW #1 files

---
+++ Homework1/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Java Programming using the command line
+
+Faith Winford
+*/
+
+public class HelloWorld
+{
+	public static void main (String [] args)
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ Homework1/Lyrics.java
@@ -0,0 +1,55 @@
+/*
+Write a program that prints out the lyrics to one or several verses of your favorite song (you can
+use java statements like System.out.println(“….”);). Be sure to include comments in the program
+indicating the title of the song and the author. Pay attention to formatting using the following
+codes:
+\n line break
+\t tab
+\" double quote
+\' single quote
+\\ backslash
+	
+
+Faith Winford
+*/
+
+public class Lyrics
+{
+	public static void main (String [] args)
+	{
+		// prints title and date of the song
+		System.out.println("Broadcast | 'Tears in the Typing Pool' Song Lyrics \n");
+
+		// prints verse 1 of the song
+		System.out.println("[Verse 1]");
+		System.out.println("Succumb to the line");
+		System.out.println("The finishing time");
+		System.out.println("The long distance runner");
+		System.out.println("Has stopped on the corner");
+		System.out.println("But I won't give up");
+		System.out.println("Although I've stopped too \n");
+
+		// prints hook of the song
+		System.out.println("[Hook]");
+		System.out.println("Before the end of me and you");
+		System.out.println("The patchwork explains");
+		System.out.println("The land is unchanged \n");
+
+		// prints verse 2 of the song
+		System.out.println("[Verse 2]");
+		System.out.println("Interpret the rooms");
+		System.out.println("My tears in the typing pool");
+		System.out.println("The letters are sighing");
+		System.out.println("The ink is still drying");
+		System.out.println("I told you the truth");
+		System.out.println("And now I sigh too \n");
+
+		// prints hook of the song
+		System.out.println("[Hook]");
+		System.out.println("The page turns on me and you");
+		System.out.println("Across that white plain");
+		System.out.println("The land is unchanged");
+
+	}
+
+}
